941ecbe4e28fd1591f29079021984cd307e426ec
Zenko <Zenko@46901.no-reply.drupal.org>
2012-02-25 11:26:18 +0100

    Issue #1447972 by Zenko: Wrapped jquery function handler.

--- back_to_top.js
+++ back_to_top.js
@@ -1,18 +1,18 @@
-Drupal.behaviors.backtotop = {
-  attach: function(context) {
-      (function($) {
-      $("body").append("<div id='backtotop'>"+Drupal.t("Back to Top")+"</div>");
-      $(window).scroll(function() {
-        if($(this).scrollTop() > 100) {
-          $('#backtotop').fadeIn();	
-        } else {
-          $('#backtotop').fadeOut();
-        }
-      });
-  
-      $('#backtotop').click(function() {
-        $('body,html').animate({scrollTop:0},1200,'easeOutQuart');
-      });	
-    })(jQuery);
-  }
-};
+(function ($) {
+	Drupal.behaviors.backtotop = {
+		attach: function(context) {
+			$("body").append("<div id='backtotop'>"+Drupal.t("Back to Top")+"</div>");
+			$(window).scroll(function() {
+				if($(this).scrollTop() > 100) {
+					$('#backtotop').fadeIn();	
+				} else {
+					$('#backtotop').fadeOut();
+				}
+			});
+
+			$('#backtotop').click(function() {
+				$('body,html').animate({scrollTop:0},1200,'easeOutQuart');
+			});	
+		}
+	};
+})(jQuery);
