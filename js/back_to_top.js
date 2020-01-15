a680af9d4c4f09af62a7ff97237131d2e7e5c82c
Mattias Axelsson <mattias.axelsson@happiness.se>
2020-01-15 15:48:45 +0100

    Issue #3005096 by valross.nu, leon340, kaicyee, acke: Not accessible for tabbing users

--- js/back_to_top.js
+++ js/back_to_top.js
@@ -4,7 +4,7 @@
 			var exist= jQuery('#backtotop').length;
       if(exist == 0) {
         $("body", context).once(function() {
-          $(this).append("<div id='backtotop'>"+Drupal.t(Drupal.settings.back_to_top.back_to_top_button_text)+"</div>");
+          $(this).append("<button id='backtotop'>"+Drupal.t(Drupal.settings.back_to_top.back_to_top_button_text)+"</button>");
         });
       }
 			$(window).scroll(function() {
