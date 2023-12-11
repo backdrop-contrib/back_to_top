1871d31d86fdd43f47e817ca1f7cf14ec3aa99d9
laryn <laryn@kragtbakker.com>
2023-12-11 16:19:48 -0600

    Issue 18: Refactor JS; ensure Enter triggers Back to Top correctly.

--- js/back_to_top.js
+++ js/back_to_top.js
@@ -3,44 +3,76 @@
  * Javascript for the Back To Top module.
  */
 
-(function($) {
+(function ($) {
 
-Backdrop.behaviors.backToTop = {
-  attach: function(context, settings) {
+  var scrollTo = function (to, duration) {
+    var element = document.scrollingElement || document.documentElement,
+      start = element.scrollTop,
+      change = to - start,
+      startTs = performance.now(),
+      easeOutQuart = function (t, b, c, d) {
+        t /= d;
+        t--;
+        return -c * (t * t * t * t - 1) + b;
+      },
+      animateScroll = function (ts) {
+        var currentTime = ts - startTs;
+        element.scrollTop = parseInt(easeOutQuart(currentTime, start, change, duration));
+        if (currentTime < duration) {
+          frame = requestAnimationFrame(animateScroll);
+        } else {
+          element.scrollTop = to;
+        }
+      };
+    requestAnimationFrame(animateScroll);
+  };
 
-    var $settings = Backdrop.settings.back_to_top;
-    var $title = ($settings.title) ? Backdrop.t($settings.text) : '';
-    var $page = $('html, body');
+  var $settings = Backdrop.settings.back_to_top;
+  var $title = ($settings.title) ? Backdrop.t($settings.text) : '';
 
-    // Add button.
-    if (!$('#backtotop').length) {
-      $('body').append('<button id="backtotop" class="' + $settings.type + '" title="' + $title + '">' + Backdrop.t($settings.text) + '</button>');
+  Backdrop.behaviors.backToTop = {
+    attach: function (context, settings) {
+      var exist = $('#backtotop').length;
+      if (exist == 0) {
+        $('body').once('backtotop', context).each(function () {
+          $(this).append("<button id='backtotop' aria-label='" + Backdrop.t("Back to top") + "' title='" + $title + "' class='" + $settings.type + "'>" + Backdrop.t($settings.text) + "</button>");
+        });
+      }
+
+      backToTop();
+      $(window).scroll(function () {
+        backToTop();
+      });
+
+      $('#backtotop').once('backtotop', context).each(function () {
+        $(this).click(function () {
+          $("html, body").bind("scroll mousedown DOMMouseScroll mousewheel keyup", function () {
+            window.cancelAnimationFrame(frame);
+          });
+          scrollTo(0, 1200);
+        });
+        $(this).keyup(function (event) {
+          if (event.keyCode === 13) {
+            event.preventDefault();
+
+            $("#backtotop").click();
+          }
+        });
+
+      });
+
+      /**
+       * Hide show back to top links.
+       */
+      function backToTop() {
+        if ($(window).scrollTop() > $settings.distance) {
+          $('#backtotop').fadeIn();
+        } else {
+          $('#backtotop').fadeOut();
+        }
+      }
     }
-
-    // Fade button in & out on scroll.
-    $(window).scroll(function() {
-      if ($(this).scrollTop() > $settings.distance) {
-        $('#backtotop').fadeIn();
-      }
-      else {
-        $('#backtotop').stop(true).fadeOut();
-      }
-    });
-
-    // Scroll to top on button click.
-    $('#backtotop').click(function() {
-      $page.bind('scroll mousedown DOMMouseScroll mousewheel keyup', function() {
-        $page.stop();
-      });
-
-      $page.animate({ scrollTop: 0 }, 500, function() {
-        $page.unbind('scroll mousedown DOMMouseScroll mousewheel keyup');
-      });
-
-      return false;
-    });
-
-  }
-};
+  };
 
 })(jQuery);
+
